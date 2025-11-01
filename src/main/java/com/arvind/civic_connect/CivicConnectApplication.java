08c599fe746c315945efa3ffd95346aea4591482
Arvind-Sabarinathan <arvind.personal2004@gmail.com>
2025-11-01 16:09:27 +0530

    first commit

---
+++ src/main/java/com/arvind/civic_connect/CivicConnectApplication.java
@@ -0,0 +1,13 @@
+package com.arvind.civic_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicConnectApplication.class, args);
+	}
+
+}
